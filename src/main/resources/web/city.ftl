5451b83e9bf14cbf5c0a7def2a4b1bd514ab4380
wanghouping <wanghouping@startdt.com>
2018-08-20 16:57:28 +0800

    清空

---
+++ src/main/resources/web/city.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<body>
+City: ${cityName}! <br>
+Q:Why I like? <br>
+A:${description}!
+</body>
+
+</html>
---
+++ src/test/java/com/owl/Main.java
@@ -0,0 +1,13 @@
+package com.owl;
+
+/**
+ * Created by houping wang on 2018/8/16
+ *
+ * @author houping wang
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
